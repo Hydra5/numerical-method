b8ac041b15153263972f7d250c91131c29370e19
Hydra <34763387+Hydra5@users.noreply.github.com>
2018-06-28 13:40:12 +0800

    Add files via upload

---
+++ Secant.cpp
@@ -0,0 +1,35 @@
+# include <stdio.h>
+# include <math.h>
+#define esp 10e-6
+ 
+double f(double x){
+     return (x*x-3*x+2);
+}
+ 
+ 
+double  solve(double x0,double x1){
+     double x;
+    
+     int i=0;
+     while(fabs(x1-x0)>esp)
+     {
+            x=x1-f(x1)*(x1-x0)/(f(x1)-f(x0));
+            x0=x1;
+            x1=x;
+            i++;
+     }
+     //printf("运算次数为 i=%d\n",i);
+    
+     return (x1);
+}
+int main ()
+{
+     double m,n,result;
+    
+     printf("please input m n: \n");
+     scanf("%lf%lf",&m,&n);
+    
+     result=solve(m,n);
+    
+    printf("result=% 6.3f\n",result);
+}
